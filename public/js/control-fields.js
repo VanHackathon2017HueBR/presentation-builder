29040d5df5e8e3bd2394ae6bc93ef95e549ee96d
Carolina Darski <carolina.darski@sap.com>
2017-04-09 00:24:16 -0300

    added pdf on landing page

--- public/js/control-fields.js
+++ public/js/control-fields.js
@@ -2,3 +2,14 @@ function openRegistration(){
   $('div.login-container').fadeIn(1000).removeClass('hidden');
   $('#start').hide();
 }
+
+function getFile(){
+  $('#new-pdf').click();
+}
+
+function changePDF(button){
+  $("#upload-file-info").html($(button).val());
+  $("#file").hide();
+  $("#success").show();
+  $("#div-start").show();
+}
